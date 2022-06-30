c304f1a19d846f64752e8b2883c2096578bd0cb0
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-06-30 21:08:42 +0530

    Create area of a tringle.c

---
+++ area of a tringle.c
@@ -0,0 +1,10 @@
+// To find the area of a tringle
+#include<stdio.h>
+int main(){
+float a,b;
+printf("enter number");
+scanf("%f%f",&a,&b);
+float area = (a*b)/2;
+printf("area is : %f",area);
+return 0;
+}
