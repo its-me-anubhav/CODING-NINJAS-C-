a90888222b317521d909950ec78853c3ae13475c
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-07-11 22:08:46 +0530

    Add files via upload

---
+++ CODING-NINJAS-CPP/addition.cpp
@@ -0,0 +1,15 @@
+// can change any intiger value and output wiil be changed 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i= 10;
+    char c = 'a';
+
+    int o = i + c;
+    char d = i + c;
+    cout << o << endl;
+    cout << d << endl;
+}
+
+
