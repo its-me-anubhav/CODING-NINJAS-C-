646003f82c2f17ef6587b20625035f227230dd16
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-07-13 01:01:19 +0530

    Add files via upload

---
+++ CODING-NINJAS-CPP/CharPattern1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    /*  Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+    	int n;
+	cin >> n;
+	int i = 1;
+	while (i <= n) {
+				int j = 1;
+		while (j <= i) {
+			char ch = 'A' + j - 1;
+		cout << ch;
+		ch++;
+				j++;
+		}
+		cout << endl;
+		i++;
+	}
+}
+
+
