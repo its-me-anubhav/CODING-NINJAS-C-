5036cf4314033f0928b0f2ad404754ef4b123b59
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-07-16 10:42:48 +0530

    Add files via upload

---
+++ CODING-NINJAS-CPP/for_loop_pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout <<"Enter the number"<< endl;
+    cin >> n;
+
+   for(int i = 1; i <= n ; i++) {
+
+    cout << i++ << endl;
+    // if we use while loop
+
+
+     // int j = 1;
+      //while ( j <= n) {
+   // cout << j++ << endl;
+    //j++
+
+    
+      }
+    }
