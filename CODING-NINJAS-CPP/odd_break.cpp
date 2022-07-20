37bb62e0d722f63a4cfc5c3cbb18e78d171e065d
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-07-20 22:30:46 +0530

    Add files via upload

---
+++ CODING-NINJAS-CPP/odd_break.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    /* Read input as specified in the question.
+	 * Print output as specified in the question.
+	 */
+    int n;
+    cin >> n;
+  int i =1;
+    while (i < n) {
+        if ( i == 9,15) {
+            i++;
+            cout << i+1<< endl;
+            continue;
+        }
+     else {
+        i++;
+        cout << i;
+     }  
+cout << i << endl;
+i++;
+     
+    }
+}
